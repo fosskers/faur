2997baede410079609bd03d8bb59f0fdaf79a9f7
Colin Woodbury <colin@fosskers.ca>
2022-03-23 16:46:12 -0700

    feat(api): multiple package names

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 use warp::Filter;
 
 #[derive(Deserialize)]
 struct Query {
-    name: String,
+    #[serde(deserialize_with = "commas")]
+    name: Vec<String>,
     by: Option<By>,
 }
 
@@ -14,13 +15,22 @@ enum By {
     Dep,
 }
 
+fn commas<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let v = s.split(',').map(|s| s.to_string()).collect();
+    Ok(v)
+}
+
 #[tokio::main]
 async fn main() {
     let search = warp::get()
         .and(warp::path("packages"))
         .and(warp::query::<Query>())
         .map(|q: Query| {
-            let msg = format!("You searched for: {} (by {:?})", q.name, q.by);
+            let msg = format!("You searched for: {:?} (by {:?})", q.name, q.by);
             warp::reply::json(&msg)
         });
 
