52ca4892f1731db27c75a9583565ec41a90f4a1c
Colin Woodbury <colin@fosskers.ca>
2023-06-16 10:49:16 +0900

    feat: indexing by "provided" package identities

--- src/faur.clj
+++ src/faur.clj
@@ -15,10 +15,45 @@
        io/reader
        (#(json/parse-stream % true))))
 
+(defn packages-by-name
+  "Given a list of all packages, yield a map that indexes them by their `:Name`
+  field."
+  [packages]
+  (transduce (map (fn [pkg] [(:Name pkg) pkg])) conj {} packages))
+
 (comment
   (->> db-file
        parse-packages
-       first))
+       packages-by-name
+       (#(get % "aura-bin"))))
+
+(defn packages-by-provider
+  "Given a list of all packages, yield a map that indexes them by the package
+  'identities' that they provide."
+  [packages]
+  (reduce (fn [prov-map pkg]
+            (let [provides (:Provides pkg)]
+              (if (or (nil? provides)
+                      (empty? provides))
+                (update prov-map
+                        (:Name pkg)
+                        (fn [set]
+                          (let [set (or set #{})]
+                            (conj set (:Name pkg)))))
+                (reduce (fn [provs provided]
+                          (update provs
+                                  provided
+                                  (fn [set]
+                                    (let [set (or set #{})]
+                                      (conj set (:Name pkg))))))
+                        prov-map provides))))
+          {} packages))
+
+(comment
+  (->> db-file
+       parse-packages
+       packages-by-provider
+       (#(get % "emacs"))))
 
 (defn same-package?
   "Are two packages logically the same?"
