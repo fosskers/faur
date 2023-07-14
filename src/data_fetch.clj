b592035ecc1d7d02c3d811cf192c74f508707fae
Colin Woodbury <colin@fosskers.ca>
2023-07-14 17:25:03 +0900

    feat: periodic data fetching and decompression
    I still haven't hooked up the DB refresh yet.

--- src/data_fetch.clj
+++ src/data_fetch.clj
@@ -1,31 +1,35 @@
 (ns data-fetch
   (:require [clojure.java.io :as io]
-            [taoensso.timbre :refer [info]]))
+            [taoensso.timbre :refer [info]]
+            [babashka.process :refer [sh]]))
+
+(def seconds-in-hour 3600000)
 
 (def db-file "packages-meta-ext-v1.json")
 
-(def seconds-in-hour 3600000)
+(def tarball (format "%s.gz" db-file))
 
 (def tarball-url
   "The location we expect to be able to download the tarball of all current
   package data."
-  (format "https://aur.archlinux.org/%s.gz" db-file))
+  (format "https://aur.archlinux.org/%s" tarball))
 
 (defn fetch
   "Make a shell call to `wget` to download a fresh copy of the JSON data."
   []
-  ())
+  (sh ["wget" "--quiet" tarball-url]))
 
 (defn unpack
   "Decompress a tarball downloaded by `wget`."
   []
-  ())
+  (sh ["gzip" "--quiet" "--force" "--decompress" tarball]))
 
 (defn endless-update []
   (loop []
     (info "Fetching updated package data.")
     (fetch)
     (unpack)
+    (info "Data refreshed. Sleeping...")
     (Thread/sleep seconds-in-hour)
     (recur)))
 
@@ -40,3 +44,7 @@
     (info "Local data already exists. Sleeping...")
     (Thread/sleep seconds-in-hour))
   (endless-update))
+
+(comment
+  (def fut (future (endless-update)))
+  (future-cancel fut))
